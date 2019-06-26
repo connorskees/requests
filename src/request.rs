e5f81fed99d17527308f9147d3aaa870a8df9c70
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-06-26 10:34:25 -0400

    Add response parsing

--- src/request.rs
+++ src/request.rs
@@ -1,5 +1,10 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
+use std::io::{BufReader};
 use std::net::{TcpStream, SocketAddr};
+
+use regex::Regex;
+use url::{Url, ParseError};
 
 #[path = "utils/structures.rs"]
 pub mod structures;
@@ -7,41 +12,95 @@ pub mod structures;
 #[path = "response.rs"]
 mod response;
 
+#[path = "utils/macros.rs"]
+#[macro_use]
+mod macros;
+
 pub struct Request {
     method: String,
     url: String,
     headers: structures::Headers,
+    prepared_request: String,
     // body: String,
 }
 
 impl Request {
-    pub fn new(method: String, url: String, headers: structures::Headers/*, body: String*/) -> Request {
-        return Request {
+    pub fn new(method: String, url: String, headers: HashMap<String, String>/*, body: String*/) -> Result<Request, ParseError> {
+        let mut headers = default_headers().update(headers);
+        let parsed_url = Url::parse(&url)?;
+        headers.insert(String::from("Host"), String::from(parsed_url.host_str().unwrap()));
+        return Ok(Request {
             method: method,
-            url: url,
+            url: String::from(parsed_url.path()),
             headers: headers,
+            prepared_request: String::from("")
             // body
-        }
+        })
     }
 
-    pub fn prepare(self) -> String {
-        format!(
+    pub fn prepare(&mut self) {
+        let req = format!(
             "{} {} HTTP/1.0\n{}\n\n",
             self.method.to_uppercase(),
             self.url,
-            self.headers.key_value_as_string().join("\n")
-        )
+            self.headers.as_string()
+        );
+        self.prepared_request = req;
     }
 
-    // pub fn send(self, prepared_request: String) -> response::Response {
-    //     let address = &SocketAddr::from(([127, 0, 0, 1], 3000));
-    //     let mut stream = TcpStream::connect(address).expect("Couldn't connect");
-    //     let result = stream.write(request.prepare().as_bytes());
-    //     println!("{}", result.unwrap());
-    //     let mut buffer = Vec::new();
-        
-    //     let response = stream.read_to_end(&mut buffer);
-    //     println!("{:?}", std::str::from_utf8(&buffer).unwrap());
-    //     Ok(())
-    // }
+    pub fn send(&self) -> response::Response {
+        let address = &SocketAddr::from(([127, 0, 0, 1], 3000));
+        let mut stream = TcpStream::connect(address).expect("Couldn't connect");
+        let result = stream.write(self.prepared_request.as_bytes());
+        // let mut buffer = Vec::new();
+
+        let f = BufReader::new(stream);
+
+        let mut currently_parsing = &Parse::FirstLine;
+        let mut response = response::Response::new_blank();
+
+        for line in f.lines() {
+            let this_line = line.unwrap();
+            match currently_parsing {
+                &Parse::FirstLine => {
+                    let first_line = Regex::new(r"(?P<version>HTTP/1.\d+) (?P<reponse_status>\d{3}) (?P<reason>[^\r\n]+)").unwrap();
+                    let caps = first_line.captures(&this_line).unwrap();
+                    let version = String::from(caps.name("version").unwrap().as_str());
+                    let reponse_status = String::from(caps.name("reponse_status").unwrap().as_str());
+                    let reason = String::from(caps.name("reason").unwrap().as_str());
+                    response.version = version;
+                    response.reponse_status = reponse_status;
+                    response.reason = reason;
+                    currently_parsing = &Parse::Header;
+                },
+                &Parse::Header => {
+                    if this_line == "" {
+                        currently_parsing = &Parse::Body;
+                        continue
+                    }
+                    let headers_regex = Regex::new(r"(?P<key>[^:]+):\s*(?P<value>[^\r\n]+)").unwrap();
+                    if headers_regex.is_match(&this_line) {
+                        let caps = headers_regex.captures(&this_line).unwrap();
+                        let key = String::from(caps.name("key").unwrap().as_str());
+                        let value = String::from(caps.name("value").unwrap().as_str());
+                        response.headers.insert(key, value);
+                    }
+                },
+                &Parse::Body => {
+                    continue
+                }
+            }
+        }
+        response
+
+        // let response = stream.read_to_end(&mut buffer);
+        // println!("{:?}", std::str::from_utf8(&buffer).unwrap());
+        // return self.parse_response(std::str::from_utf8(&buffer).unwrap());
+    }
+}
+
+enum Parse {
+    FirstLine,
+    Header,
+    Body
 }
